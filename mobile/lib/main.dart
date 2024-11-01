534d1da382f67df4a3c3d79fa15c61c6f59e10f8
Renan <renancvr777@gmail.com>
2024-11-01 02:00:33 -0300

    formulario cliente

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:gerenciador_pedidos/screens/clientes-formulario.dart';
 import 'package:gerenciador_pedidos/screens/clientes-listagem.dart';
 import 'package:gerenciador_pedidos/screens/home.dart';
 import 'package:gerenciador_pedidos/screens/pedidos.listagem.dart';
 
 void main() {
   runApp(
-    //const Home()
-    const ClientesListagem()
-    );
+      //const Home()
+      ClientesFormulario());
 }
--- mobile/lib/screens/clientes-formulario.dart
+++ mobile/lib/screens/clientes-formulario.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:gerenciador_pedidos/components/navigation-bar.component.dart';
 
 class ClientesFormulario extends StatelessWidget {
-  final TextEditingController idCliente;
+  final TextEditingController idCliente = TextEditingController();
+  final TextEditingController nome = TextEditingController();
+  final TextEditingController dataCadastro = TextEditingController();
+  final TextEditingController ruaEndereco = TextEditingController();
+  final TextEditingController numeroEndereco = TextEditingController();
 
-  const ClientesFormulario({super.key, required this.idCliente});
+  ClientesFormulario({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,15 +19,104 @@ class ClientesFormulario extends StatelessWidget {
           backgroundColor: Colors.blue,
         ),
         body: Padding(
-          padding: const EdgeInsets.fromLTRB(170.0, 0.0, 170.0, 0.0),
+          padding: const EdgeInsets.fromLTRB(170.0, 25.0, 170.0, 0.0),
           child: Column(
             children: [
               TextField(
                 controller: idCliente,
-
+                keyboardType: TextInputType.number,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                ),
+                decoration: const InputDecoration(
+                    icon: Icon(Icons.insert_drive_file),
+                    labelText: 'ID',
+                    hintText: '000'),
               ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: nome,
+                keyboardType: TextInputType.number,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                ),
+                decoration: const InputDecoration(
+                    icon: Icon(Icons.account_circle_outlined),
+                    labelText: 'Nome',
+                    hintText: 'Fulano'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: dataCadastro,
+                keyboardType: TextInputType.number,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                ),
+                decoration: const InputDecoration(
+                    icon: Icon(Icons.date_range),
+                    labelText: 'Data de Cadastro',
+                    hintText: '00/00/0000'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50.0, 0, 50.0, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: ruaEndereco,
+                        keyboardType: TextInputType.number,
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                        ),
+                        decoration: const InputDecoration(
+                            icon: Icon(Icons.assistant_photo_outlined),
+                            labelText: 'Rua',
+                            hintText: ''),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 50,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        controller: numeroEndereco,
+                        keyboardType: TextInputType.number,
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                        ),
+                        decoration: const InputDecoration(
+                            icon: Icon(Icons.numbers),
+                            labelText: 'Número',
+                            hintText: '0000'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: const ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Colors.blue)),
+                  child: const Text(
+                    'Confirmar Cadastro',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ]),
             ],
-          )
+          ),
         ),
         bottomNavigationBar: const NavigationBarComponent(),
       ),
